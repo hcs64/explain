537cb54e864c1e2fd0a05b135d60bc3d1acba309
Adam Gashlin <agashlin@gmail.com>
2012-12-21 17:17:42 -0500

    split out utility functions gathered from the web

---
+++ util.js
@@ -0,0 +1,68 @@
+// utilities library
+
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+
+// requestAnimationFrame polyfill by Erik Möller
+// fixes from Paul Irish and Tino Zijdel
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = 
+          window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+ 
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+function supports_html5_storage() {
+    try {
+        return 'localStorage' in window && window['localStorage'] !== null;
+    } catch (e) {
+        return false;
+    }
+}
+
+// on the recommendation of HTML5: Up and Running
+function supports_canvas() {
+    return !!document.createElement('canvas').getContext;
+}
+
+function supports_canvas_text() {
+    if (!supports_canvas()) { return false; }
+    var dummy_canvas = document.createElement('canvas');
+    var context = dummy_canvas.getContext('2d');
+    return typeof context.fillText == 'function';
+}
+
+function getCursorPosition(el, ev) {
+    var x, y;
+    if (ev.pageX != undefined && ev.pageY != undefined) {
+        x = ev.pageX;
+        y = ev.pageY;
+    } else {
+        x = ev.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+        y = ev.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+    }
+
+    x -= el.offsetLeft;
+    y -= el.offsetTop;
+
+    return {x: x, y: y};
+}
+
