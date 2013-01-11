ee116d9ed48aad2ee51b82369a02a68f8d3800c4
Adam Gashlin <agashlin@gmail.com>
2013-01-11 17:27:14 -0500

    error reporting

--- java/src/AwtTest.java
+++ java/src/AwtTest.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.lang.reflect.UndeclaredThrowableException;
 import bsh.Interpreter;
 import bsh.EvalError;
 import bsh.BshClassManager;
@@ -21,6 +22,7 @@ public class AwtTest extends java.applet.Applet implements Runnable {
     Image buffer_image;
     Graphics buffer_graphics;
     Rectangle r = new Rectangle(0,0,0,0);
+    Font error_font = new Font("Monospaced", Font.PLAIN, 14);
 
     enum CodeState {
         RUNNING,
@@ -33,6 +35,9 @@ public class AwtTest extends java.applet.Applet implements Runnable {
     String code;
     EPLTalker epl;
     EPLTextState server_state;
+
+    int err_line;
+    String err_str;
 
     public void init() {
         buffer_image = null;
@@ -62,6 +67,7 @@ public class AwtTest extends java.applet.Applet implements Runnable {
             e.printStackTrace();
         }
 
+        err_str = "Waiting for initial text...";
         code = "";
         server_state = null;
         code_state = CodeState.HALTED;
@@ -123,6 +129,8 @@ public class AwtTest extends java.applet.Applet implements Runnable {
             } catch (EvalError e) {
                 System.out.println("eval error "+e.toString()+", not accepting new code:\n" + new_code);
 
+                err_str = e.toString();
+                err_line = -1;
                 code_state = CodeState.PARSE_ERROR;
 
                 // re-eval old code
@@ -131,6 +139,8 @@ public class AwtTest extends java.applet.Applet implements Runnable {
                 } catch (EvalError e2) {
                     System.out.println("error " + e2.toString() + " reverting to previously ok code " + code);
 
+                    err_str = "!";
+                    err_line = -1;
                     code_state = CodeState.HALTED;
                 }
             }
@@ -140,14 +150,46 @@ public class AwtTest extends java.applet.Applet implements Runnable {
             try {
                 bsh.set("gw", gw);
                 bsh_renderable.render(gw);
+            } catch (UndeclaredThrowableException e) {
+                System.out.println("Runtime error, HALTING");
+                e.printStackTrace();
+
+                Throwable cause = e.getCause();
+                if (cause instanceof EvalError) {
+                    err_line = ((EvalError)cause).getErrorLineNumber();
+                } else {
+                    err_line = -1;
+                }
+
+                err_str = cause.toString();
+
+                code_state = CodeState.HALTED;
             } catch (Exception e) {
                 System.out.println("Runtime error, HALTING");
                 e.printStackTrace();
 
+                err_line = -1;
+                err_str = "!!!";
+
                 code_state = CodeState.HALTED;
             }
-
-            g.drawImage(buffer_image, 0, 0, this);
         }
+
+        if (code_state == CodeState.PARSE_ERROR || code_state == CodeState.HALTED) {
+
+            buffer_graphics.setClip(null);
+            buffer_graphics.setFont(error_font);
+
+            buffer_graphics.setColor(Color.BLACK);
+            buffer_graphics.fillRect(0,0,r.width,60);
+            buffer_graphics.setColor(Color.RED);
+            if (err_line != -1) {
+                buffer_graphics.drawString("Line: " + String.valueOf(err_line), 25, 25);
+            }
+            buffer_graphics.drawString(err_str, 25, 45);
+        }
+
+        g.drawImage(buffer_image, 0, 0, this);
+
     }
 }
